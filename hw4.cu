f54d4041a959d3b6234da635169d5d341cf22b55
jdk81 <jdk81@login0.crc.pitt.edu>
2019-04-18 13:37:48 -0400

    updated

--- hw4.cu
+++ hw4.cu
@@ -7,29 +7,22 @@
 #define DOMAIN_END 1.0
 
 
-__global__ void trap_appx(int iters, int max_iters, double dx, double *global_sum) {
+__global__ void trap_appx(int trapezoids, double dx, double *global_sum) {
   // Get our global thread ID
   __shared__ double shared_sum;
   shared_sum = 0;
   double local_sum = 0.0;
-  int id = (blockIdx.x * blockDim.x + threadIdx.x) * iters;
-  double x = id * dx;
-  double p1 = 0.0;
-  double p2 = FUNCTION(x);
-  __syncthreads();
+  int id = (blockIdx.x * blockDim.x + threadIdx.x);
 
-  int i;
-  for (i = id; (i < (id + iters)) && (i < max_iters); i++) {
-    x += dx;
-    p1 = p2;
-    p2 = FUNCTION(x);
-    local_sum += ((p2 + p1) / 2) * dx;
+  if (id >= trapezoids) {
+    return;
   }
+  double x = id * dx;
+  
+  local_sum += ( FUNCTION(x) + FUNCTION((x + dx))) * 0.5 * dx;
 
-  //printf("%d's local sum is %2.6f\n", id, local_sum);
   atomicAdd(&shared_sum, local_sum);
   __syncthreads();
-  //printf("%d's shared sum is %2.6f\n", id, shared_sum);
 
   if (threadIdx.x == 0) {
     atomicAdd(global_sum, shared_sum);
@@ -45,24 +38,24 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
-  int total_threads = atoi(argv[1]);
-  int grid_size = total_threads / 256;
-  if (total_threads % 256 > 0) {
-    total_threads++;
+  int threads = atoi(argv[1]);
+  if (threads > 1024) {
+    printf("Error: max of 1024 threads.\n");
+    return -1;
+  }
+  else if (threads < 1) {
+    printf("Error: min of 1 thread\n");
+    return -1;
   }
 
   int trapezoids = 1000000;
-  int iters_per_thread = trapezoids / total_threads;
-  if (trapezoids % total_threads > 0) {
-    iters_per_thread++;
+  int total_blocks = trapezoids / threads;
+  if (trapezoids % threads > 0) {
+    total_blocks++;
   }
 
   double incr = (DOMAIN_END - DOMAIN_START) / ((double) trapezoids);
 
-  if (total_threads < 1) {
-    printf("Error: number of threads must be greater than zero.\n");
-    return -1;
-  }
 
   double *h_sum = (double *) calloc(1, sizeof(double));
   double *d_sum = NULL;
@@ -77,7 +70,7 @@ int main(int argc, char *argv[]) {
 
   cudaEventRecord(before);
 
-  trap_appx <<< grid_size, 256 >>> (iters_per_thread, trapezoids, incr, d_sum);
+  trap_appx <<< total_blocks, threads >>> (trapezoids, incr, d_sum);
   cudaMemcpy(h_sum, d_sum, sizeof(double), cudaMemcpyDeviceToHost);
   cudaDeviceSynchronize();
 
